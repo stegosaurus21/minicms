32d361ef34ae4c0ee86f45e5a14452fa1dee8334
Jason Liu <z5416164@ad.unsw.edu.au>
2024-02-18 21:55:15 +1100

    add(infra): add build script and improve Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -5,32 +5,18 @@ COPY ./config/supervisord.conf /etc/supervisor/supervisord.conf
 
 WORKDIR /minicms
 
-COPY ./backend/package-lock.json ./backend/
-COPY ./backend/package.json ./backend/
-COPY ./frontend/package-lock.json ./frontend/
-COPY ./frontend/package.json ./frontend/
+COPY ./backend/package-lock.json ./
+COPY ./backend/package.json ./
 
-WORKDIR /minicms/backend
-RUN npm ci
-WORKDIR /minicms/frontend
 RUN npm ci
 
-WORKDIR /minicms
-
-COPY ./backend ./backend
-COPY ./backend/.docker.env ./backend/.env
-
-WORKDIR /minicms/backend
-
+COPY ./backend/prisma ./prisma
+COPY ./backend/.docker.env ./.env
 RUN npx prisma generate
 RUN npx prisma migrate deploy
 RUN npx prisma db seed
 
-WORKDIR /minicms
-COPY ./frontend ./frontend
-
-WORKDIR /minicms/frontend
-RUN npm run build
+COPY ./backend .
 
 CMD ["/usr/bin/supervisord", "-c", "/etc/supervisor/supervisord.conf"]
 
---
+++ build.sh
@@ -0,0 +1,50 @@
+if [ $# -lt 1 ] || [ $# -gt 2 ] || ([ $# -eq 2 ] && [ "$2" != "--publish" ]); then
+    echo "Usage: ./build.sh <version> [--publish]"
+    exit 1
+fi
+
+if [ -n "$(git tag | grep "$1")" ]; then
+    echo -n "Version $1 already exists, overwrite? (y/n): "
+    read resp
+    if [ "$resp" != "y" ]; then
+        echo "Aborted. The most recent version is $(git tag | tac | head -n 1)."
+        exit 1
+    fi
+    overwrite=1
+else
+    overwrite=0
+fi
+
+echo "Building frontend..."
+cd frontend || exit 1
+npm run build
+if [ $? -ne 0 ]; then
+    echo "An error occurred while building the frontend. The build was aborted."
+    exit 1
+fi
+
+echo "Building image..."
+cd ..
+docker build . -t "jl58261/minicms:$1"
+if [ $? -ne 0 ]; then
+    echo "An error occurred while building the image. The build was aborted."
+    exit 1
+fi
+
+if [ "$2" == "--publish" ]; then
+    if [ "$overwrite" == "1" ]; then
+        echo "Removing previous commit tag..."
+        git tag -d "$1"
+    fi
+    
+    echo "Tagging commit..."
+    git tag "$1"
+
+    echo "Publishing to GitHub..."
+    git push
+    
+    echo "Publishing to Docker Hub..."
+    docker push "jl58261/minicms:$1"
+fi
+
+echo "Done."
